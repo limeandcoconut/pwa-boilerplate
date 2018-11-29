b33bd807f661db8c794ab2067034048bdc7f0ed4
Brian Whicheloe <brian@whicheloe.us>
2018-11-28 16:24:47 -0800

    Enable Vue Analytics by default with dummy UA-ID

--- client/entry_client.js
+++ client/entry_client.js
@@ -1,6 +1,7 @@
-// import Vue from 'vue'
-// import VueAnalytics from 'vue-analytics'
+import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import createApp from './create_app.js'
+import {gaDevId, gaProductionId} from '../config.js'
 
 const {app, store, router} = createApp()
 
@@ -8,33 +9,16 @@ if (window.__INITIAL_STATE__) {
     store.replaceState(window.__INITIAL_STATE__)
 }
 
-// TODO
-// CHANGEME
-// let gaID
-// if (process.env.NODE_ENV === 'development') {
-//     gaID = 'UA-XXX-X' // Dev gaID
-// } else {
-//     gaID = 'UA-XXX-X' // Live gaID
-// }
-
-// Vue.use(VueAnalytics, {
-//     id: gaID,
-//     router,
-//     ignoreRoutes: ['admin'],
-//     ecommerce: {
-//         enabled: true,
-//         enhanced: true,
-//     },
-//     // debug: {
-//     //     enabled: true,
-//     //     trace: false,
-//     //     sendHitTask: true,
-//     // },
-// })
+const gaId = (process.env.NODE_ENV === 'development') ? gaDevId : gaProductionId
+Vue.use(VueAnalytics, {
+    id: gaId,
+    router,
+})
 
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope)
         }, function(err) {
             console.log('ServiceWorker registration failed: ', err)
--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const frontendPort = 3005
 const siteName = 'My Cool Site'
+const gaDevId = 'UA-93531536-4'
+const gaProductionId = ''
 
 module.exports = {
     frontendPort,
     siteName,
+    gaDevId,
+    gaProductionId,
 }
